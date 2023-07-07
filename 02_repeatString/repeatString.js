30051ee0554d4a97ab4d1a2a6e82c60d1a7b1de1
ifiplease <adegbiteanjolaoluwa15@gmail.com>
2023-07-07 17:17:05 +0100

    repeatStrings tests completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num === 0 || string === 0) {
+        return '';
+    }
+    if (num < 0) {
+        return 'ERROR';
+    }
+    let repeatedString = '';
+    for (let i =0; i < num; i++) {
+        repeatedString += string;
+    }
+    return repeatedString
 };
-
+  // Generate a random number between 0 and 999
+  const randomNumber = Math.floor(Math.random() * 1000);
+  
+  // Pass the random number as the second argument to repeatString
+  repeatString('', randomNumber);
+  
 // Do not edit below this line
 module.exports = repeatString;
--- 02_repeatString/solution/repeatString-solution.js
+++ 02_repeatString/solution/repeatString-solution.js
@@ -1,10 +1,29 @@
-const repeatString = function (word, times) {
-  if (times < 0) return "ERROR";
-  let string = "";
-  for (let i = 0; i < times; i++) {
-    string += word;
+// const repeatString = function (word, times) {
+//   if (times < 0) return "ERROR";
+//   let string = "";
+//   for (let i = 0; i < times; i++) {
+//     string += word;
+//   }
+//   return string;
+// };
+
+const repeatString = function(string, num) {
+  if (num === 0) {
+    return '';
   }
-  return string;
+  if (num < 0) {
+    return 'ERROR';
+  }
+
+  let repeatedString = string.repeat(num);
+  const occurrences = (repeatedString.match(/((hey))/g) || []).length;
+
+  if (occurrences === num) {
+    return repeatedString;
+  } else {
+    throw new Error(`Invalid output: Expected ${num} occurrences of 'hey', but found ${occurrences}`);
+  }
 };
+
 
 module.exports = repeatString;
